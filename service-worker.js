6dbb0391f465945d06d25ae2811d7459e2038766
robothro-png <robothro@gmail.com>
2026-04-13 12:23:50 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,22 @@
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open("planner").then(cache=>{
-      return cache.addAll(["./"]);
-    })
-  );
+const CACHE="planner-cache-v1";
+
+const FILES=[
+"./",
+"./index.html",
+"./manifest.json",
+"./icon.png"
+];
+
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e=>{
+self.addEventListener("activate",e=>{
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch",e=>{
   e.respondWith(
     caches.match(e.request).then(r=>r||fetch(e.request))
   );
